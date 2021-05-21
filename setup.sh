08454adc0c180e647eda10759a9b9b4a6ff74a12
Nathan Franke <natfra@pm.me>
2021-05-20 20:36:16 -0500

    wip3

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 cd "$(dirname "$0")/mpv-build"
 
-echo "--enable-libmpv-static" > mpv_options
+echo "--enable-static" > ffmpeg_options
+echo "--disable-gl
+--enable-libmpv-static" > mpv_options
 
 CFLAGS="-fPIC" ./rebuild $@
 rm *_options
